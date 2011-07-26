f62c197f261913d2e8edd50cf14a2187cd806d12
srohatgi <sumeet_rohatgi@hotmail.com>
2011-07-26 16:42:02 -0700

    updated /upload to save to content/uploaded_files directory

--- node/index.js
+++ node/index.js
@@ -1,18 +1,30 @@
-var server = require("./server.js");
-var request_handler = require("./request_handler.js");
+var server = require("./server.js"),
+    request_handler = require("./request_handler.js");
 
-// dependency injections start
+////////////////////////////////////////////
+// dependency injections properties start
 
 var listening_port = 8888;
 
 var handle = {};
+
+// all handlers share the doc root
+handle.doc_root = "content"; 
+// here is where we store any uploaded, modified files
+handle.file_save_path = "content/uploaded_files"; 
+// this is where the web upload form posts
+handle.form_action = "/upload"; 
+
 handle["/"] = request_handler.index;
 handle["/index"] = request_handler.index;
 handle["/upload"] = request_handler.upload;
+
 handle["/set_upload"] = request_handler.set_upload;
+
 handle["/404"] = request_handler.unknown;
 
-// dependency injections stop
+// dependency injections properties stop
+////////////////////////////////////////////
 
 // main event loop
 server.start(listening_port, handle, handle["/404"]);
--- node/request_handler.js
+++ node/request_handler.js
@@ -4,21 +4,23 @@ var url = require("url"),
 
 // api for introspecting server methods
 function index(req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write("upload: form post of file(s)\n");
-  res.write("set_upload: sets up a html form for uploading file from browser");
+  console.log("upload path: "+this.doc_root);
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write('<a href="/upload">upload</a>: form post of file(s)<br/>');
+  res.write('<a href="/set_upload">set_upload</a>: sets up a html form for uploading file from browser');
   res.end();
 }
 
 // api for setting up uploading of files
-// needs 'action' information to be injected
+// needs 'form_action' information to be injected
 function set_upload(req, res) {
+  console.log("form_action: "+this.form_action);
   var body = '<html>'+
     '<head>'+
     '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<form action="' + this.form_action + '" enctype="multipart/form-data" method="post">'+
     '<input type="file" name="upload">'+
     '<input type="submit" value="Upload file" />'+
     '</form>'+
@@ -31,15 +33,17 @@ function set_upload(req, res) {
 }
 
 // api for uploading files
-// needs 'path' information to be injected
+// needs 'file_save_path' information to be injected
 function upload(req, res) {
   console.log("Inside Request handler 'upload'");
 
   var form = new formidable.IncomingForm();
+  var dir = this.file_save_path;
+
   console.log("about to parse");
   form.parse(req, function(error, fields, files) {
-    console.log("parsing done");
-    fs.renameSync(files.upload.path, "/tmp/test.png");
+    console.log("parsing done, "+ files.upload.path + " saved to: " + dir);
+    fs.renameSync(files.upload.path, dir + "/test.png");
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write("received image:<br/>");
     res.end();
